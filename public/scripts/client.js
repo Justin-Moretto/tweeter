c3460fb03ff5adfc6c3596292590917ee2aa7963
justin-moretto <JustinMorettoMTL@gmail.com>
2020-11-19 14:29:40 +0000

    impletemented fetching tweets with ajax

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,45 +4,57 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-// Test / driver code (temporary). Eventually will get this from the server.
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
-    },
-  "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-  "created_at": 1461116232227
-}
-
-const renderTweets = function(tweets) {
-  for (const tweet of tweets) {
-    createTweetElement(tweet);
-  }
-}
 
 
-const createTweetElement = (tweetData) => {
-  return `<article class="tweet">
-          <div class="tweeter">
-            <div>${tweetData.user.name}</div>
-            <div>${tweetData.user.handle}</div>
-          </div>
-          <p class="tweeter">${tweetData.content.text}</p>
-          <footer>
-            <div>10 days ago</div>
-            <div>Flag, Retweet, Like</div>
-          </footer>
-          </article>`
-};
+//const $tweet = createTweetElement(tweetData);
 
-const $tweet = createTweetElement(tweetData);
 
-// Test / driver code (temporary)
-console.log($tweet); // to see what it looks like
+
 
 $(document).ready(() => {
-  $('#timeline').append($tweet);
+  
+  const createTweetElement = (tweetData) => {
+    return `<article class="tweet">
+            <div class="tweeter">
+              <div>${tweetData.user.name}</div>
+              <div>${tweetData.user.handle}</div>
+            </div>
+            <p class="tweeter">${tweetData.content.text}</p>
+            <footer>
+              <div>${tweetData.created_at}</div>
+              <div>Flag, Retweet, Like</div>
+            </footer>
+            </article>`
+  };
+  
+  const renderTweets = function(tweets) {
+    for (const tweet of tweets) {
+      $('#timeline').prepend(createTweetElement(tweet));
+    }
+  }
+
+  //submit tweet to the database when submit buttom is clicked
+  $('form').on('submit', event => {
+    event.preventDefault();
+    $.ajax({
+      url: '/tweets',
+      method: 'POST',
+      data: $('form').serialize()
+    }).then(() => {
+      loadTweets();
+    })
+  });
+
+  //Get all tweets and load them to page in order, starting with most recent
+  const loadTweets = () => {
+    $.ajax({
+      url: '/tweets',
+      method: 'GET',
+      dataType: 'JSON',
+    }).then((res) => {
+      $('#timeline').empty();
+      renderTweets(res);
+    });
+  }
+  loadTweets();
 })
